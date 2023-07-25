675e20d23632ced8a5d3ad3ec7724568231db81c
paulsikabalu <sikabalu@outlook.com>
2023-07-25 01:30:55 -0700

    sorted

--- 102-main.c
+++ 102-main.c
@@ -9,10 +9,7 @@
  */
 int main(void)
 {
- int array[] = {0, 1, 2, 3, 2, 1, 0, 3};
-/**    int array[] = {19, 48, 99, 71, 19, 52, 96, 73, 86, 7};*/
-/**    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};*/
-/**    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};*/
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
