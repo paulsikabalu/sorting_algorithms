3ba5c925b714fc5bbf91053d6fbeef841a895742
sikabalupaul <4929210@gmail.com>
2023-07-25 11:01:19 +0300

    Sort

--- 105-main.c
+++ 105-main.c
@@ -9,7 +9,10 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    int array[] = {6, 8, 3};
+/**    int array[] = {48, 99, 71, 13, 52, 96, 73, 86, 7, 8, 22, 25, 82, 63, 44, 57};*/
+/**    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};*/
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
